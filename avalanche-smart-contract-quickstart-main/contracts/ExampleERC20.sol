f0dedea5ebf02e8e5f67c26f8487f4660cb53def
DebuGGaJav <95142758+DebuGGaJav@users.noreply.github.com>
2021-11-27 20:53:34 +0300

    Update ExampleERC20.sol

--- avalanche-smart-contract-quickstart-main/contracts/ExampleERC20.sol
+++ avalanche-smart-contract-quickstart-main/contracts/ExampleERC20.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ExampleERC20 is ERC20, Ownable {
-  string private TOKEN_NAME = "Example ERC20 Token";
-  string private TOKEN_SYMBOL = "XMPL";
+  string private TOKEN_NAME = "EROL Token";
+  string private TOKEN_SYMBOL = "EROL";
 
-  uint256 private constant TOTAL_SUPPLY = 123456789;
+  uint256 private constant TOTAL_SUPPLY = 100000000;
 
   constructor()ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
     _mint(msg.sender, TOTAL_SUPPLY);
